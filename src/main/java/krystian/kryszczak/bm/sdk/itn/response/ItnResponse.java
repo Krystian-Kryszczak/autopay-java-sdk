5590c672e9e23b11fc53aa3832f97fa0cda02c69
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-04-23 22:07:54 +0200

    ItnResponse updated.

--- src/main/java/krystian/kryszczak/bm/sdk/itn/response/ItnResponse.java
+++ src/main/java/krystian/kryszczak/bm/sdk/itn/response/ItnResponse.java
@@ -4,35 +4,43 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import krystian.kryszczak.bm.sdk.BlueMediaConfiguration;
 import krystian.kryszczak.bm.sdk.hash.HashGenerator;
 import krystian.kryszczak.bm.sdk.itn.Itn;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
-import java.util.Map;
 
-public record ItnResponse(int serviceID, @NotNull TransactionsConfirmations transactionsConfirmations, @NotNull String hash) implements Serializable {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ItnResponse implements Serializable {
+    private final int serviceID;
+    private final @NotNull TransactionsConfirmations transactionsConfirmations;
+    private final @NotNull String hash;
+
     private static final String STATUS_CONFIRMED = "CONFIRMED";
     private static final String STATUS_NOT_CONFIRMED = "NOTCONFIRMED";
 
-    public static ItnResponse create(@NotNull Itn itn, boolean transactionConfirmed, @NotNull BlueMediaConfiguration configuration) { // TODO
+    public static @NotNull ItnResponse create(@NotNull Itn itn, boolean transactionConfirmed, @NotNull BlueMediaConfiguration configuration) {
         final var confirmation = transactionConfirmed ? STATUS_CONFIRMED : STATUS_NOT_CONFIRMED;
 
-        final var hashData = new Object[]{
-                configuration.getServiceId(),
-                itn.getOrderId(),
-                confirmation
+        final String orderId = itn.getOrderId();
+
+        final var hashData = new Object[] {
+            configuration.getServiceId(),
+            orderId,
+            confirmation
         };
 
-        final var itnResponseData = Map.of(
-                "serviceID", configuration.getServiceId(),
-                "transactionsConfirmations", Map.of(
-                        //
-                ),
-                "hash", HashGenerator.instance.generateHash(hashData, configuration)
+        return new ItnResponse(
+            configuration.getServiceId(),
+            new TransactionsConfirmations(
+                new TransactionConfirmed(
+                    orderId,
+                    confirmation
+                )
+            ),
+            HashGenerator.instance.generateHash(hashData, configuration)
         );
-
-        //
-        return null;
     }
 
     @SneakyThrows
