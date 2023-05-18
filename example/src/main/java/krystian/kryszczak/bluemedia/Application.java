1ff05a6543a18020c8b0142be141f5530bab431d
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-05-18 20:51:36 +0200

    Created example module.

---
+++ example/src/main/java/krystian/kryszczak/bluemedia/Application.java
@@ -0,0 +1,10 @@
+package krystian.kryszczak.bluemedia;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
